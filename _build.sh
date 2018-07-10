9c71d0167ebf206c12c061535d4385af997d810a
willbowditch <will@bowdit.ch>
2018-07-10 10:28:48 +0100

    Update lint process

--- _build.sh
+++ _build.sh
@@ -1,6 +1,7 @@
 # Lint the Rmd files, if they fail then return an error in Travis
 # note: this is a bit of a workaround as lintR doesn't seem to support
 #       book linting out of the box.
+failed_lint=false
 for f in *.Rmd; do
 lint_result=$(Rscript -e "lintr::lint('$f')")
 echo $lint_result
@@ -10,8 +11,16 @@ echo $lint_result
     echo "file $f passed"
   else
     echo "file $f failed lint"
+    failed_lint=true
   fi
 done
 
 # Build the book!
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+
+# If it failed the lint then error!
+if $failed_lint;
+then
+  echo "At least one file failed lintR (see above)"
+  exit 1
+fi
